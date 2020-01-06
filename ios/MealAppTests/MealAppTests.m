0e16030369525110e2fb18432a410a55c4e43405
Trinh Thanh Phuong <iphuongtt@gmail.com>
2020-01-06 14:13:10 +0700

    rename folders in ios from TestApp... to MealApp...

--- ios/MealAppTests/MealAppTests.m
+++ ios/MealAppTests/MealAppTests.m
@@ -14,11 +14,11 @@
 #define TIMEOUT_SECONDS 600
 #define TEXT_TO_LOOK_FOR @"Welcome to React"
 
-@interface TestAppTests : XCTestCase
+@interface MealAppTests : XCTestCase
 
 @end
 
-@implementation TestAppTests
+@implementation MealAppTests
 
 - (BOOL)findSubviewInView:(UIView *)view matching:(BOOL(^)(UIView *view))test
 {
