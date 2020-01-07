26e1dbceaf695fbc8c165cd91419f72cf62b5fbf
Trinh Thanh Phuong <iphuongtt@gmail.com>
2020-01-07 15:21:48 +0700

    Adding Meal model and data

---
+++ models/meal.ts
@@ -0,0 +1,33 @@
+export class Meal {
+  constructor(
+    public id: string,
+    public categoryId: string[],
+    public title: string,
+    public affordability: string,
+    public complexity: string,
+    public imageUrl: string,
+    public duration: number,
+    public ingredients: string[],
+    public steps: string[],
+    public isGlutenFree: boolean,
+    public isVegan: boolean,
+    public isVegetarian: boolean,
+    public isLactoseFree: boolean,
+  ) {
+    this.id = id;
+    this.categoryId = categoryId;
+    this.title = title;
+    this.affordability = affordability;
+    this.complexity = complexity;
+    this.imageUrl = imageUrl;
+    this.duration = duration;
+    this.ingredients = ingredients;
+    this.steps = steps;
+    this.isGlutenFree = isGlutenFree;
+    this.isVegan = isVegan;
+    this.isVegetarian = isVegetarian;
+    this.isLactoseFree = isLactoseFree;
+  }
+}
+
+export default Meal;
