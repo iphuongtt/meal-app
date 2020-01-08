c2aa74c5ea59c92df88032c47a154637de106ccb
Trinh Thanh Phuong <iphuongtt@gmail.com>
2020-01-08 08:46:17 +0700

    Fixing the shadows

--- components/CategoryGridTile.tsx
+++ components/CategoryGridTile.tsx
@@ -41,15 +41,19 @@ const styles = StyleSheet.create({
     margin: 15,
     height: 150,
     borderRadius: 10,
-    overflow: 'hidden',
+    overflow:
+      Platform.OS === 'android' && Platform.Version >= 21
+        ? 'hidden'
+        : 'visible',
+    elevation: 5,
+    shadowColor: 'black',
+    shadowOpacity: 0.26,
+    shadowOffset: {width: 0, height: 2},
+    shadowRadius: 10,
   },
   container: {
     flex: 1,
     borderRadius: 10,
-    shadowColor: 'black',
-    shadowOpacity: 0.26,
-    shadowOffset: {width: 0, height: 2},
-    elevation: 3,
     padding: 10,
     justifyContent: 'flex-end',
     alignItems: 'flex-end',
