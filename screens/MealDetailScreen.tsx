ed84e8dbab2ba5923033b470aebe0a26499cd4e4
Trinh Thanh Phuong <iphuongtt@gmail.com>
2020-01-09 13:56:08 +0700

    Adding the Meal Detail Screen Content

--- screens/MealDetailScreen.tsx
+++ screens/MealDetailScreen.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
-import {View, Text, StyleSheet, Button} from 'react-native';
+import {View, Text, StyleSheet, Button, ScrollView, Image} from 'react-native';
 import {NavigationStackScreenComponent} from 'react-navigation-stack';
-import {CustomHeaderButton} from '../components';
+import {CustomHeaderButton, DefaultText} from '../components';
 import {HeaderButtons, Item} from 'react-navigation-header-buttons';
 import {MEALS} from '../data/dummy-data';
 
@@ -9,10 +9,28 @@ export const MealDetailScreen: NavigationStackScreenComponent = props => {
   const mealId = props.navigation.getParam('mealId');
   const selectedMeal = MEALS.find(meal => meal.id === mealId);
   return (
-    <View style={styles.screen}>
-      <Text>{selectedMeal.title}</Text>
-      <Button title="Go Back" onPress={() => props.navigation.goBack()} />
-    </View>
+    <ScrollView>
+      <Image source={{uri: selectedMeal.imageUrl}} style={styles.image} />
+      <View style={styles.detail}>
+        <DefaultText>{selectedMeal.duration}m</DefaultText>
+        <DefaultText>{selectedMeal.complexity}</DefaultText>
+        <DefaultText>{selectedMeal.affordability}</DefaultText>
+      </View>
+      <Text style={styles.title}>Ingredients</Text>
+      <Text>List of ingredients</Text>
+      {selectedMeal.ingredients.map(ingredient => (
+        <Text key={ingredient}>{ingredient}</Text>
+      ))}
+      <Text style={styles.title}>Steps</Text>
+      <Text>List of steps</Text>
+      {selectedMeal.steps.map(step => (
+        <Text key={step}>{step}</Text>
+      ))}
+      <View style={styles.screen}>
+        <Text>{selectedMeal.title}</Text>
+        <Button title="Go Back" onPress={() => props.navigation.goBack()} />
+      </View>
+    </ScrollView>
   );
 };
 
@@ -32,6 +50,20 @@ MealDetailScreen.navigationOptions = ({navigation}) => {
 const styles = StyleSheet.create({
   screen: {
     flex: 1,
+  },
+  title: {
+    fontFamily: 'OpenSans-Bold',
+    fontSize: 22,
+    textAlign: 'center',
+  },
+  image: {
+    width: '100%',
+    height: 200,
+  },
+  detail: {
+    flexDirection: 'row',
+    padding: 15,
+    justifyContent: 'space-around',
   },
 });
 
